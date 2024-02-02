e1070f52d82faa84d1f2961ee6a08b7b68638140
Vadim <vadimb@xpertnetz.com>
2024-02-03 00:55:07 +0300

    Updated to ver 0.4

--- predict.py
+++ predict.py
@@ -5,7 +5,7 @@ from PIL import Image
 from pinecone import Pinecone
 from io import BytesIO
 from cog import BasePredictor, Path, Input
-
+import time
 
 class Predictor(BasePredictor):
 
@@ -23,15 +23,14 @@ class Predictor(BasePredictor):
         else:
             self.device = "cpu"
 
+        self.tokenizer = open_clip.get_tokenizer('coca_ViT-L-14')
+
         self.im_model, self._, self.im_transform = open_clip.create_model_and_transforms(
             model_name="coca_ViT-L-14",
             pretrained="mscoco_finetuned_laion2B-s13B-b90k"
         )
 
-        if self.device == "cuda":
-            self.im_model.cuda().eval()
-        else:
-            self.im_model.eval()
+        self.im_model.to(self.device).eval()
 
     def get_caption(self, image, model, transform) -> str:
         im = transform(image).unsqueeze(0).to(self.device)
@@ -44,20 +43,35 @@ class Predictor(BasePredictor):
         caption = open_clip.decode(generated[0])
         return caption
 
-    def predict(self, image: Path = Input(description="Input image")) -> str:
+    def predict(self,
+                image: Path = Input(description="Input image"),
+                use_advanced_processing: bool = False,
+                use_simple: bool = False
+                ) -> str:
+        if use_simple:
+            time.sleep(0.2)
+            return ""
+
         with open(image, "rb") as f:
             image_bytes = f.read()
 
         new_image = (Image.open(BytesIO(image_bytes))).convert("RGB")
         # preprocessed_new_image = self.preprocess(new_image).unsqueeze(0).to(self.device)
 
+        if use_advanced_processing:
+            new_size = (512, 512)
+        else:
+            new_size = (256, 256)
+
+        new_image = new_image.resize(new_size, Image.Resampling.LANCZOS)
+
         # with torch.no_grad():
         #    new_image_embedding = self.model.encode_image(preprocessed_new_image)
         image_caption = self.get_caption(new_image, self.im_model, self.im_transform)
         image_caption = (image_caption.replace("<start_of_text>", "").replace("<end_of_text>", "")).strip()
-        print(image_caption)
+        #print(image_caption)
 
-        text_inputs = tokenizer.tokenize(image_caption).to(self.device)
+        text_inputs = self.tokenizer(image_caption).to(self.device)
 
         # Preprocess the new image and compute its embedding
         preprocessed_new_image = self.im_transform(new_image).unsqueeze(0).to(self.device)
@@ -75,3 +89,15 @@ class Predictor(BasePredictor):
         )
 
         return str(search_results)
+
+if __name__ == "__main__":
+    predictor = Predictor()
+    predictor.setup()
+
+    for i in range(5):
+        start_time = time.time()
+        print(predictor.predict("test.jpg", use_simple=False))
+        end_time = time.time()
+        duration = end_time - start_time
+        print(f"Время выполнения: {duration} секунд.")
+    print("\n")
