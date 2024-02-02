e217b8db015fec11b21950387d0ec254a393db14
Vadim <vadimb@xpertnetz.com>
2024-02-02 18:41:12 +0300

    Updated to ver 0.3

--- predict.py
+++ predict.py
@@ -2,43 +2,46 @@ import torch
 import open_clip
 from open_clip import tokenizer
 from PIL import Image
-import pinecone
-import os
-import requests
+from pinecone import Pinecone
 from io import BytesIO
-from lxml import etree
-from tqdm import tqdm
 from cog import BasePredictor, Path, Input
-import json
+
 
 class Predictor(BasePredictor):
+
     def setup(self):
         api_key = "73f171c4-1136-4c95-90e2-ea857b7e364d"
-        pinecone.init(api_key, environment="us-central1-gcp")
+        pc = Pinecone(api_key=api_key)
 
         index_name = "clip-embeddings"
-        self.index = pinecone.Index(index_name)
+        self.index = pc.Index(index_name)
 
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        #self.model, self.preprocess = clip.load("ViT-B/32", device=self.device)
+        if torch.cuda.is_available():
+            self.device = "cuda"
+        elif torch.backends.mps.is_available():
+            self.device = "mps"
+        else:
+            self.device = "cpu"
+
         self.im_model, self._, self.im_transform = open_clip.create_model_and_transforms(
             model_name="coca_ViT-L-14",
             pretrained="mscoco_finetuned_laion2B-s13B-b90k"
         )
+
         if self.device == "cuda":
             self.im_model.cuda().eval()
         else:
             self.im_model.eval()
 
-    def get_caption(self, image, model, transform):
-        im = transform(image).unsqueeze(0).to(self.device)  # cuda()
+    def get_caption(self, image, model, transform) -> str:
+        im = transform(image).unsqueeze(0).to(self.device)
         with torch.no_grad():
             if self.device == "cuda":
                 with torch.cuda.amp.autocast():
                     generated = model.generate(im)
             else:
                 generated = model.generate(im)
-        caption = open_clip.decode(generated[0])  # .split("")[0].replace("", "")[:-2]
+        caption = open_clip.decode(generated[0])
         return caption
 
     def predict(self, image: Path = Input(description="Input image")) -> str:
@@ -46,9 +49,9 @@ class Predictor(BasePredictor):
             image_bytes = f.read()
 
         new_image = (Image.open(BytesIO(image_bytes))).convert("RGB")
-        #preprocessed_new_image = self.preprocess(new_image).unsqueeze(0).to(self.device)
+        # preprocessed_new_image = self.preprocess(new_image).unsqueeze(0).to(self.device)
 
-        #with torch.no_grad():
+        # with torch.no_grad():
         #    new_image_embedding = self.model.encode_image(preprocessed_new_image)
         image_caption = self.get_caption(new_image, self.im_model, self.im_transform)
         image_caption = (image_caption.replace("<start_of_text>", "").replace("<end_of_text>", "")).strip()
@@ -71,19 +74,4 @@ class Predictor(BasePredictor):
             include_metadata=True
         )
 
-        #if search_results is None:
-        #   return 'IsNone'
-        #else:
-        #   print(search_results)
-        #   return 'is not None'
         return str(search_results)
-        # Convert search_results into a JSON-serializable dictionary with the desired format
-        #json_search_results = {
-        #    "matches": [
-        #        {
-        #            "id": query_id,
-        #            "metadata": metadata,
-        #            "score": round(100 - distance * 100, 6),  # Convert distance to a percentage score
-        #            "values": []
-        #        }
-        #        for query_id, metadata, distance in search_results.items()
